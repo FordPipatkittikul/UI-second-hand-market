2fe041f3a7a7cc3a64b3db975345ee044ba7bdb3
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-05-24 15:46:47 -0400

    set up routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,40 @@
-import Navbar from "./components/navbar/Navbar"
 import HomePage from "./pages/homePage/homePage"
+import ListPage from "./pages/listPage/listPage";
+import Layout from "./pages/layout/layout";
+import SinglePage from "./pages/singlePage/singlePage";
 
-import "./layout.scss"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path: "/",
+          element: <HomePage/>
+        },
+        {
+          path: "/list",
+          element: <ListPage/>
+        },
+        {
+          path: "/:id",
+          element: <SinglePage/>
+        }
+      ]
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-      
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
