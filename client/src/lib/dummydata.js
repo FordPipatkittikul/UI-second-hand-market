99458f974aba4e836486e09563162d247b863b69
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-05-25 16:42:21 -0400

    update data

--- client/src/lib/dummydata.js
+++ client/src/lib/dummydata.js
@@ -10,56 +10,48 @@ export const listData = [
     title: "apple",
     img: "https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1000,
-    address: "456 Park Avenue, London",
   },
   {
     id: 2,
     title: "tomato",
     img: "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1500,
-    address: "789 Oxford Street, London",
   },
   {
     id: 3,
     title: "soup",
     img: "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 800,
-    address: "101 Baker Street, London",
   },
   {
     id: 4,
     title: "zz",
     img: "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1000,
-    address: "234 Kingsway, London,",
   },
   {
     id: 5,
     title: "5",
     img: "https://images.pexels.com/photos/276625/pexels-photo-276625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1500,
-    address: "567 Victoria Road, London",
   },
   {
     id: 6,
     title: "6",
     img: "https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 800,
-    address: "890 Regent Street, London",
   },
   {
     id: 7,
     title: "7",
     img: "https://images.pexels.com/photos/2029667/pexels-photo-2029667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1000,
-    address: "112 Piccadilly, London",
   },
   {
     id: 8,
     title: "8",
     img: "https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     price: 1500,
-    address: "8765 Main High Street, London",
   },
 ];
 
@@ -74,11 +66,19 @@ export const singlePostData = {
     "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   ],
   description:
-    "Future alike hill pull picture swim magic chain seed engineer nest outer raise bound easy poetry gain loud weigh me recognize farmer bare danger. actually put square leg vessels earth engine matter key cup indeed body film century shut place environment were stage vertical roof bottom lady function breeze darkness beside tin view local breathe carbon swam declared magnet escape has from pile apart route coffee storm someone hold space use ahead sheep jungle closely natural attached part top grain your grade trade corn salmon trouble new bend most teacher range anybody every seat fifteen eventually",
+    "Adjectives — descriptive words that modify nouns — often come under fire for their cluttering quality, but often it’s quality, not quantity, that is the issue. Plenty of tired adjectives are available to spoil a good sentence, but when you find just the right word for the job, enrichment ensues. Practice precision when you select words. Here’s a list of adjectives:",
+  phone:
+    "319 471-1427",
+  email:
+    "bigmom@uiowa.edu"
 };
 
 export const userData = {
   id: 1,
-  name: "Fred baf",
+  name: "big mom",
   img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  phone:
+  "319 471-1427",
+  email:
+  "bigmom@uiowa.edu"
 };
