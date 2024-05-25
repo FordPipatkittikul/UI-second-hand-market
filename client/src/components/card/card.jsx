ac1202d8db4cc824da4e99e7bc9e26efb6bb88fe
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-05-25 16:48:40 -0400

    comment save features. we can implement this feature later

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -12,30 +12,23 @@ function Card({item}){
         <h2 className='title'>
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
-        {/* <p className='address'>
-          <img src="/pin.png" alt=""/>
-          <span>{item.address}</span>
-        </p> */}
         <div className='middle'>
           <p className='price'>$ {item.price}</p>
         </div>
         <div className='bottom'>
-          {/* <div className='features'>
-            <div className='feature'>
-            
-            </div>
-            <div className='feature'>
-            
-            </div>
-          </div> */}
-          <div className='icons'>
+          
+          {/* TODO : User can save Item for future 
+            WILL Implement this features later If I want to
+          */}
+          {/* <div className='icons'>
             <div className='icon'>
                <img src='/save.png' alt=''/>
             </div>
             <div className='icon'>
                <img src='/chat.png' alt=''/>
             </div>
-          </div>
+          </div> */}
+
         </div>
       </div>
     </div>
