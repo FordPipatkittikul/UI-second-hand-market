68ccf87746f0edfef1f1eb56d1e8e2dec9c358e2
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-05-25 00:07:13 -0400

    draft for singlePage

--- client/src/pages/singlePage/singlePage.jsx
+++ client/src/pages/singlePage/singlePage.jsx
@@ -1,8 +1,54 @@
+import Slider from '../../components/slider/Slider'
 import './singlePage.scss'
+import { singlePostData, userData } from '../../lib/dummydata'
+
+import ParticlesBg from 'particles-bg'
 
 function SinglePage(){
   return (
-    <div className='singlePage'>SinglePage</div>
+    <div className='singlePage'>
+
+      <div className='details'>
+        <div className="wrapper">
+          <Slider images={singlePostData.images}/>
+
+          <div className='info'>
+
+            <div className='top'>
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className='user'>
+                <img src={userData.img} alt=''></img>
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            
+            <div className='description'>
+                Description: {singlePostData.description}
+            </div>
+
+            <div className='icons'>
+              <div className='icon'>
+                  <img src='/save.png' alt=''/>
+              </div>
+              <div className='icon'>
+                  <img src='/chat.png' alt=''/>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+      <div className='features'>
+        <ParticlesBg/>
+        <div className="wrapper"></div>
+      </div>
+
+    </div>
   )
 }
 
