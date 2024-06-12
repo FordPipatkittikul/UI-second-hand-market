470720373eb711c6373c4bb67cdeb61b4396b174
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-06-11 22:44:14 -0400

    change to render

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://ui-second-hand-market.onrender.com/api",
     withCredentials: true
 });
 
 export default apiRequest
+
+//http://localhost:8000/api
+//https://ui-second-hand-market.onrender.com/api
