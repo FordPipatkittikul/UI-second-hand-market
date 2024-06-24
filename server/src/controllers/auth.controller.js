26bfc7acee04c9d623aa241d9a2c160fab6f9ab4
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-06-24 11:50:02 -0400

    refactor(auth) : creating userExist,generateToken and createAndSaveUserTODB function for reuseable purpose

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,21 +1,16 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import prisma from "../lib/prisma.js";
 
+import { userExist,generateToken,createAndSaveUserTODB } from "../models/auth.model.js";
 
 export async function login(req,res){
     const {username, password} = req.body;
 
     try{
 
-        // CHECK IF USER DOES EXISTS IN DB
-        const user = await prisma.user.findUnique({
-            where:{username}
-        });
+        const user = await userExist(username);
         if(!user){
             return res.status(401).json({msg: "Invalid Credential"})
         };
-
 
         // CHECK PASSWORD 
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
@@ -23,22 +18,20 @@ export async function login(req,res){
             return res.status(401).json({msg: "Invalid Credential"})
         }
 
-        
-        // GENERATE TOKEN AND STORE IN COOKIES AND SEND IT TO A USER
         const {password:userPassword, ...userInfo} = user
         const week = 1000 * 60 * 60 * 24 * 7; // milisec to min to hour to day to week
-        const token = jwt.sign({
-            id:user.id,
-            isAdmin: false
-        }, process.env.JWT_SECRET_KEY, {expiresIn:week}); // JWT token include user id and expire it in one week
+        const token = generateToken(user,week);
+
+        // CREATING COOKIE FOR USER 
         return res.cookie("token",token, {
-            httpOnly:true,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
             maxAge: week
         }).status(200).json(userInfo)
 
     }catch(err){
+        console.log(err)
         return res.status(500).json({msg: "Fail to login"});
     }
 }
@@ -50,18 +43,11 @@ export async function register(req,res){
     try{
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // CREATE A NEW USER AND SAVE TO DB
-        const newUser = await prisma.user.create({
-            data: {
-                username,
-                email,
-                password: hashedPassword,
-                phone
-            }
-        });
+        createAndSaveUserTODB(username,email,hashedPassword,phone)
 
         return res.status(201).json({msg: "User created successfully"});
     }catch(err){
+        console.log(err)
         return res.status(500).json({msg: "Fail to create user"});
     }
 
---
+++ server/src/models/auth.model.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
+
+export async function userExist(username) {
+    const user = await prisma.user.findUnique({
+        where:{username}
+    });
+    return user
+}
+
+export function generateToken(user,week){
+    const token = jwt.sign({
+        id:user.id,
+        isAdmin: false
+    }, process.env.JWT_SECRET_KEY, {expiresIn:week}); // JWT token include user id and expire it in one week
+    return token
+}
+
+export async function createAndSaveUserTODB(username,email,hashedPassword,phone){
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashedPassword,
+            phone
+        }
+    });
+}
+
+// module.exports = {
+//     userExist,
+//     generateToken,
+// }
